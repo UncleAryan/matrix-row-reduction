2653a37e24099254579d48e6fa48b693825efd05
UncleAryan <aryanpanchal1702@gmail.com>
2025-03-10 14:55:00 -0400

    added 3 row operations

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,21 @@ public class Main {
                              {2, 7, 1, -2}};
 
     public static void main(String[] args) {
-        int pivotColumn = getPivotColumn(matrix);
-        int nonZeroEntry = getNonZeroEntry(matrix, pivotColumn);
+        //int pivotColumn = getPivotColumn(matrix);
+        //int nonZeroEntry = getNonZeroEntry(matrix, pivotColumn);
+
+        row1PlusRow2(matrix, 0, 1, 2);
 
         printMatrix(matrix);
-        System.out.println("Nonzero Pivot Entry: " + nonZeroEntry);
+        //System.out.println("Nonzero Pivot Entry: " + nonZeroEntry);
+    }
+
+    /*
+    Step 3: Use row replacement operations to create zeros in all positions below the
+    pivot.
+     */
+    public static void createZeroBelowPivot(int[][] matrix, int pivot) {
+
     }
 
     /*
@@ -50,12 +60,32 @@ public class Main {
         return -1; // a nonzero column was not found
     }
 
+    // below are all row operations to perform on augmented matrices
     public static void interchange(int matrix[][], int row1, int row2) {
         int[] tempRow = matrix[row1];
         matrix[row1] = matrix[row2];
         matrix[row2] = tempRow;
     }
 
+    public static void row1PlusRow2(int[][] matrix, int row1, int row2, int scaler) {
+        for(int i = 0; i < matrix[0].length; i++) {
+            matrix[row1][i] += matrix[row2][i] * scaler;
+        }
+    }
+
+    public static void row1MinusRow2(int[][] matrix, int row1, int row2, int scaler) {
+        for(int i = 0; i < matrix[0].length; i++) {
+            matrix[row1][i] -= matrix[row2][i] * scaler;
+        }
+    }
+
+    public static void scaleRow(int[][] matrix, int row, int scaler) {
+        for(int i = 0; i < matrix[0].length; i++) {
+            matrix[row][i] *= matrix[row][i] * scaler;
+        }
+    }
+
+    // method to print an matrix (2d array) for debugging purposes
     public static void printMatrix(int[][] matrix) {
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
